f22dd571730b33ebe1568e2d8ec4dd2f9e88bd81
Shariful Islam <mshorif3525@gmail.com>
2026-07-08 13:52:40 +0600

    add server

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const contactRoutes = require('../routes/contactRoutes');
+const { default: Message } = require('tedious/lib/message');
+
+const app = express();
+app.use(express.json());
+
+function checkAccountId(req, res, next) {
+    const accountId = Number(req.headers['account-id']);
+    if(!accountId){
+        return res.status(401).json({status:'error', Message: 'Givee header as account-id'});
+    }
+
+    req.accountId = accountId;
+    next();
+    
+}
+
+app.use('/api/contacts', checkAccountId, contactRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server runing on : http://localhost:${PORT}`);
+    
+})
+
+module.exports = app;
